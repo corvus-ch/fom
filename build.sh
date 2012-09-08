198125ca6c6d923c4735d27c2d30fb361093a24b
Christian Häusler <haeusler.christian@mac.com>
2012-09-08 10:36:13 +0200

    Custom theme
    * Created a subtheme of Corolla
    * Moved css from database to files
    
    fixed #2

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ rm -rf public
 drush make forum.make public --translations=de,fr
 
 ln -s ../../../../modules/ public/sites/all/modules/custom
+ln -s ../../../../themes/fom_theme public/sites/all/themes/fom_theme
 
 rm -r public/sites/default
 ln -s ../../default public/sites/default
